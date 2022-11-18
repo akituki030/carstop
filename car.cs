c19639442c9e6e05db35468939f588f3497afea2
akituki030 <qazasd5678@gmail.com>
2022-11-18 16:25:42 +0800

    測試2

--- car.cs
+++ car.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class car : MonoBehaviour
 {
+    float myspeed = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            myspeed = 0.2f;
+        }
+        transform.Translate(myspeed, 0, 0);
+        myspeed *= 0.98f;
     }
 }
