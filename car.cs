7d060325b8d88df9d9ae6d31f1bd08f3e4ce2cd1
akituki030 <qazasd5678@gmail.com>
2022-11-18 16:41:28 +0800

    第一次修改

--- car.cs
+++ car.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class car : MonoBehaviour
 {
     float myspeed = 0f;
+    Vector2 starPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +15,15 @@ public class car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            myspeed = 0.2f;
+            starPos = Input.mousePosition;//將點擊滑鼠時的座標記錄下來
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;//將放開滑鼠時的座標紀錄起來
+            float swipeLength = endPos.x - starPos.x;
+            myspeed = swipeLength / 500.0f;//把滑鼠滑動長度傳換成初始移動速度
         }
         transform.Translate(myspeed, 0, 0);
         myspeed *= 0.98f;
